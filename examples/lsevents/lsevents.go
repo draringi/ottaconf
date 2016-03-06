e42fe08d8a80b98bb531191d06a5d4823cfcb754
Michael Williams <draringi@draringi.net>
2016-03-06 16:19:47 -0500

    Add URL support
    Only supports http and https schemes so far, because I'm lazy.
    Non-issue though, as both BSDCan and PGCon serve the files up over https.

--- examples/lsevents/lsevents.go
+++ examples/lsevents/lsevents.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-	"github.com/draringi/ottaconf"
-	"fmt"
 	"flag"
-	"os"
+	"fmt"
+	"github.com/draringi/ottaconf"
 	"io"
+	"net/http"
+	"os"
 )
 
-
-
 type msg struct {
-	title string
-	people string
+	title     string
+	people    string
 	eventType string
 }
 
 func main() {
 	isURL := false
-	flag.BoolVar(&isURL ,"U", false, "Tells lsevents that the path provided is a URL, allowing for")
+	flag.BoolVar(&isURL, "U", false, "Tells lsevents that the path provided is a URL, allowing for")
 	flag.Parse()
 	args := flag.Args()
 	if len(args) != 1 {
@@ -29,7 +28,13 @@ func main() {
 	var source io.ReadCloser
 	var err error
 	if isURL {
-		
+		c := new(http.Client)
+		r, err := c.Get(path)
+		if err != nil {
+			fmt.Printf("Error reading data: %v\n", err)
+			return
+		}
+		source = r.Body
 	} else {
 		source, err = os.Open(path)
 		if err != nil {
@@ -46,7 +51,7 @@ func main() {
 	events := conf.Events()
 	var lenTitle, lenPeople, lenType int
 	var eventPrints []*msg
-	for _, e := range(events) {
+	for _, e := range events {
 		m := new(msg)
 		m.title = e.Title()
 		if len(m.title) > lenTitle {
@@ -54,9 +59,9 @@ func main() {
 		}
 		var peopleString string
 		peopleList := e.People()
-		for i, p := range(peopleList) {
+		for i, p := range peopleList {
 			peopleString += p.Name()
-			if i + 1 < len(peopleList) {
+			if i+1 < len(peopleList) {
 				peopleString += ", "
 			}
 		}
@@ -72,35 +77,35 @@ func main() {
 	}
 	{
 		space := lenTitle - 5
-		sep := space>>1
-		printSpace(sep+1)
+		sep := space >> 1
+		printSpace(sep + 1)
 		fmt.Printf("Title")
-		if (sep<<1) < space {
+		if (sep << 1) < space {
 			sep++
 		}
-		printSpace(sep+1)
+		printSpace(sep + 1)
 		fmt.Printf("|")
 		space = lenPeople - 6
 		sep = space >> 1
-		printSpace(sep+1)
+		printSpace(sep + 1)
 		fmt.Printf("People")
-		if (sep<<1) < space {
-                        sep++
-                }
-                printSpace(sep+1)
-                fmt.Printf("|")
+		if (sep << 1) < space {
+			sep++
+		}
+		printSpace(sep + 1)
+		fmt.Printf("|")
 		space = lenType - 4
-                sep = space >> 1
-                printSpace(sep+1)
-                fmt.Printf("Type\n")
-		printDash(lenTitle+2)
+		sep = space >> 1
+		printSpace(sep + 1)
+		fmt.Printf("Type\n")
+		printDash(lenTitle + 2)
 		fmt.Printf("+")
-		printDash(lenPeople+2)
+		printDash(lenPeople + 2)
 		fmt.Printf("+")
-		printDash(lenType+2)
+		printDash(lenType + 2)
 		fmt.Printf("\n")
 	}
-	for _, m := range(eventPrints) {
+	for _, m := range eventPrints {
 		fmt.Printf(" %s", m.title)
 		printSpace(lenTitle - len(m.title))
 		fmt.Printf(" | %s", m.people)
@@ -116,7 +121,7 @@ func printSpace(count int) {
 }
 
 func printDash(count int) {
-	for i:= 0; i < count; i++ {
+	for i := 0; i < count; i++ {
 		fmt.Printf("-")
 	}
 }
