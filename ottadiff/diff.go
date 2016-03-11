9974903085a7c6d2eeffdc7b8adde3b96e0f7a60
Michael Williams <draringi@draringi.net>
2016-03-10 19:02:01 -0500

    Added some initial documentation to ottadiff

--- ottadiff/diff.go
+++ ottadiff/diff.go
@@ -2,19 +2,25 @@ package ottadiff
 
 import "github.com/draringi/ottaconf"
 
+// ChangeType is an enumerator storing what type of change has taken place.
 type ChangeType int
 
 const (
+	// Insertation signals an addition of an object to the schedule
 	Insertation ChangeType = iota
+	// Modification signals that an object in the schedule has been modified
 	Modification
+	// Deletion signals that an object has been removed from the schedule
 	Deletion
 )
 
+// Change represents the any change within the schedule, to any object type.
 type Change interface {
 	Type() ChangeType
 	String() string
 }
 
+// Diff examines two schedules, and returns a list of changes from the old schedule to the new schedule.
 func Diff(oldSchedule, newSchedule *ottaconf.Conference) []Change {
 	changes := []Change{diffConferenceMeta(oldSchedule, newSchedule)}
 	for _, e := range newSchedule.Events() {
--- ottadiff/ottadiff.go
+++ ottadiff/ottadiff.go
@@ -1,1 +1,3 @@
+// Package ottadiff is a schedule difference calculator for pentabarf xml output.
+// It is designed to be used with the output of ottaconf's Parse function.
 package ottadiff
